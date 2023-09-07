4dcbbba54ca1a8545c26669e803539c53c1d3e46
Sue419 <suzan419@gmail.com>
2023-09-07 11:18:18 -0500

    rutas home, series y movies

--- movie/src/app/app-routing.module.ts
+++ movie/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Importa el componente Home
+import { MoviesComponent } from './movies/movies.component'; // Importa el componente Movies
+import { SeriesComponent } from './series/series.component'; // Importa el componente Series
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Ruta para la página principal (home)
+  { path: 'movies', component: MoviesComponent }, // Ruta para la página de películas (movies)
+  { path: 'series', component: SeriesComponent }, // Ruta para la página de series (series)
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
