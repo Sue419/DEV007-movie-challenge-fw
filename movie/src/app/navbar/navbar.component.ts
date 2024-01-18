47b1b53035c9546a92a3a180ddf51f445ec47c42
Sue419 <suzan419@gmail.com>
2024-01-17 23:45:59 -0500

    feat(navbar): add mobile-friendly layout

--- movie/src/app/navbar/navbar.component.ts
+++ movie/src/app/navbar/navbar.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  isNavbarVisible = false;
+
+  toggleNavbar() {
+    this.isNavbarVisible = !this.isNavbarVisible;
+  }
+
 }
